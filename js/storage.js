14cfe8b0d14f9ac22752c68cb7d729b935150311
Antonio Carlos <carlos_antonio.minga@outlook.com>
2023-05-23 10:33:43 -0300

    funções de addPosition e UpdatePosition, e atualizado setLocalStorage

--- js/storage.js
+++ js/storage.js
@@ -1,17 +1,43 @@
 'use strict';
 
 function createNewRide() {
-  const rideID = new Date.now();
+  const rideID = Date.now();
   const rideRecord = {
     data: [],
-    startTime: new Date.now(),
+    startTime: Date.now(),
     stopTime: null
   };
 
-  return setLocalStorage(rideID, rideRecord);
+  setLocalStorage(rideID, rideRecord);
+  return rideID;
 }
 
 function setLocalStorage(rideID, rideRecord) {
-  localStorage.setItem(rideID, rideRecord);
-  return rideID;
+  localStorage.setItem(rideID, JSON.stringify(rideRecord));
+}
+
+function getRideRecord(rideID) {
+  return JSON.parse(localStorage.getItem(rideID));
+}
+
+function addPosition(rideID, position) {
+  const rideRecord = getRideRecord(rideID);
+  const newData = {
+    accuracy: position.coords.accuracy,
+    altitude: position.coords.altitude,
+    altitudeAccuracy: position.coords.altitudeAccuracy,
+    heading: position.coords.heading,
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+    speed: position.coords.speed,
+    timeStamp: position.timeStamp
+  };
+  rideRecord.data.push(newData);
+  setLocalStorage(rideID, rideRecord);
+}
+
+function updateStopTime(rideID) {
+  const rideRecord = getRideRecord(rideID);
+  rideRecord.stopTime = Date.now();
+  setLocalStorage(rideID, rideRecord);
 }
