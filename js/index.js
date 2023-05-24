8b4ffc5d7d38bce16c05f89f61319ba52ea2c9bf
Antonio Carlos <carlos_antonio.minga@outlook.com>
2023-05-24 16:08:04 -0300

    funçao para listar todos itens do localstorage em tela

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
   const listRideElement = document.querySelector('#rideList');
+  loadList();
+
+  function loadList() {
+    const allRides = getAllRides();
+    if (allRides) {
+      allRides.forEach(([id, value]) => {
+        const ride = JSON.parse(value);
+        ride.id = id;
+        const itemElement = document.createElement('li');
+        itemElement.id = ride.id;
+        itemElement.innerText = ride.id;
+        listRideElement.appendChild(itemElement);
+      });
+    } else console.log('não ha itens a exibir');
+  }
 });
