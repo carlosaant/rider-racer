2e8bef1f7f780fec02569d7fe3e3e48b57740ea3
Antonio Carlos <carlos_antonio.minga@outlook.com>
2023-05-25 15:51:39 -0300

    função para localizar a cidade pelas cordenadas, usando API

--- js/index.js
+++ js/index.js
@@ -6,14 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
   function loadList() {
     const allRides = getAllRides();
     if (allRides) {
-      allRides.forEach(([id, value]) => {
+      allRides.forEach(async ([id, value]) => {
         const ride = JSON.parse(value);
+        const firstPosition = ride.data[0]; //o primeiro registro para determinar a cidade
+        const firstLocationData = await getLocationData(
+          firstPosition.longitude,
+          firstPosition.latitude
+        );
+
         ride.id = id;
         const itemElement = document.createElement('li');
         itemElement.id = ride.id;
-        itemElement.innerText = ride.id;
+        const cityDiv = document.createElement('div');
+        cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.principalSubdivision}`;
+
+        itemElement.appendChild(cityDiv);
         listRideElement.appendChild(itemElement);
       });
     } else console.log('não ha itens a exibir');
+  }
+
+  async function getLocationData(longitude, latitude) {
+    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    const response = await fetch(url);
+    return await response.json();
   }
 });
