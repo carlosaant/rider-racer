3b1cbac0b41d483e79b05aec4d16b22b295e946f
Antonio Carlos <carlos_antonio.minga@outlook.com>
2023-05-30 10:42:40 -0300

    formatDuration movido para getDuration

--- js/index.js
+++ js/index.js
@@ -92,14 +92,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function getDuration(ride) {
     const interval = ride.stopTime - ride.startTime;
+    function formatDuration(duration) {
+      const minutes = Math.floor(duration / 60000);
+      const seconds = Math.floor((duration % 60000) / 1000);
+      return `${minutes
+        .toString()
+        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
     return formatDuration(interval);
-  }
-
-  function formatDuration(duration) {
-    const minutes = Math.floor(duration / 60000);
-    const seconds = Math.floor((duration % 60000) / 1000);
-    return `${minutes
-      .toString()
-      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 });
